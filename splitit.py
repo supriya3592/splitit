3a55c5c5c00ce4076b7231ac2c5bc083b1b220ab
PS <60010701+supriya3592@users.noreply.github.com>
2024-04-02 11:14:14 +0900

    Update splitit.py

--- splitit.py
+++ splitit.py
@@ -1,6 +1,22 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
+
+
+# CSS code to hide the icon
+#We define a variable hide_github_icon_css containing the CSS code.
+hide_github_icon_css = """
+<style>
+/* Hide the Streamlit GitHub icon */
+.reportview-container .main-container > header {
+    display: none;
+}
+</style>
+"""
+# We use st.markdown to render the CSS code. Note that we set unsafe_allow_html=True to allow including HTML tags in the markdown.
+st.markdown(hide_github_icon_css, unsafe_allow_html=True)
+
+
 exurl="https://docs.google.com/spreadsheets/d/1cCvrf6drHcHTTPZhX1Fswmut7XPysyIv0Kil65UyiW4/edit?usp=sharing"
 conn = st.connection("gsheets", type=GSheetsConnection)
 
