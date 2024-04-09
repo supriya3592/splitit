82c70b86d4a8b1a8e4f20aabfd4454444d37df0e
PS <60010701+supriya3592@users.noreply.github.com>
2024-04-09 16:49:37 +0900

    Update splitit.py

--- splitit.py
+++ splitit.py
@@ -63,8 +63,8 @@ for person, balance in positive_balances.items():
 
 
 
-
+x=
 # -- PLOT DATAFRAME
-fig = px.bar(monthly_data,x='Date of expense',y='Amount',template='plotly_white',title='Mahine ka Hisab')
+fig = px.bar(monthly_data,x=monthly_data.index.name,y='Amount',template='plotly_white',title='Mahine ka Hisab')
 st.plotly_chart(fig)
 
